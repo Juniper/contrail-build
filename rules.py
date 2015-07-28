9cafd3cb217ed1005ef9b1efbd1821760ec95a30
Ananth Suryanarayana <anantha@juniper.net>
2015-07-28 15:37:41 -0700

    Partial-Bug: 1464016 Add a way to disable heap-check for unit tests
    For stress tests such as bgp_stress_test, heapcheck (pprof) enabled env is not
    suitable due to issues with performance. With this fix, HEAPCHECK is not enabled
    if NO_HEAPCHECK is set in environment
    
    Change-Id: I455d99fddbb7ed4d32fccd1e982d92a761c9c3dc

--- rules.py
+++ rules.py
@@ -38,7 +38,7 @@ def RunUnitTest(env, target, source, timeout = 180):
     ShEnv.update({env['ENV_SHLIB_PATH']: 'build/lib',
                   'DB_ITERATION_TO_YIELD': '1',
                   'TOP_OBJECT_PATH': env['TOP'][1:]})
-    heap_check = True
+    heap_check = env['ENV'].has_key('NO_HEAPCHECK') == False
     try:
         # Skip HEAPCHECK in CentOS 6.4
         subprocess.check_call("grep -q \"CentOS release 6.4\" /etc/issue 2>/dev/null", shell=True)
