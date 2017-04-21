a18e25f1fca9c2c75b02faa26189f74b87b9e61f
Santosh Gupta <sangupta@juniper.net>
2017-04-20 17:02:43 -0700

    Add env variable for snmp-conf.
    Controller and packaging changes to follow.
    
    Change-Id: Iae2be4db4a51e5201c9c91d323fd3dc4b7669b1d
    Closes-Bug: #1665778

--- rules.py
+++ rules.py
@@ -998,6 +998,7 @@ def SetupBuildEnvironment(conf):
     env['INSTALL_INIT'] = ''
     env['INSTALL_INITD'] = ''
     env['INSTALL_CONF'] = ''
+    env['INSTALL_SNMP_CONF'] = ''
     env['INSTALL_EXAMPLE'] = ''
     env['PYTHON_INSTALL_OPT'] = ''
     env['INSTALL_DOC'] = ''
@@ -1010,6 +1011,7 @@ def SetupBuildEnvironment(conf):
         env['INSTALL_INIT'] = install_root
         env['INSTALL_INITD'] = install_root
         env['INSTALL_CONF'] = install_root
+        env['INSTALL_SNMP_CONF'] = install_root
         env['INSTALL_EXAMPLE'] = install_root
         env['INSTALL_DOC'] = install_root
         env['PYTHON_INSTALL_OPT'] = '--root ' + install_root + ' '
@@ -1036,6 +1038,7 @@ def SetupBuildEnvironment(conf):
     env['INSTALL_INIT'] += '/etc/init'
     env['INSTALL_INITD'] += '/etc/init.d'
     env['INSTALL_CONF'] += '/etc/contrail'
+    env['INSTALL_SNMP_CONF'] += '/etc/snmp'
     env['INSTALL_EXAMPLE'] += '/usr/share/contrail'
     env['INSTALL_DOC'] += '/usr/share/doc'
 
