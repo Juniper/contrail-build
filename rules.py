57abd990dbeaf2256bee13a67d23d003a40e1bbb
Krzysztof Klimonda <kklimonda@syntaxhighlighted.com>
2018-02-22 18:49:10 +0100

    Generate jUnit XML reports for gtest tests
    This is an initial approach to generate jUnit XML test reports for gtest
    unit tests.
    
    Change-Id: I6a94a7a84ddeb99c598ad2cb021f3bcd956f5e4e

--- rules.py
+++ rules.py
@@ -75,6 +75,11 @@ def RunUnitTest(env, target, source, timeout = 300):
     if env['ENV'].has_key('CONTRAIL_UT_TEST_TIMEOUT'):
         timeout = int(env['ENV']['CONTRAIL_UT_TEST_TIMEOUT'])
 
+    # Enable jUnit XML report generation for GoogleTest framework
+    # XXX(kklimonda): Fake it out by adding log so it's uploaded to logs.o.o
+    junit_xml_path = os.path.splitext(logfile)[0] + ".xml.log"
+    env['ENV']['GTEST_OUTPUT'] = 'xml:' + junit_xml_path
+
     test = str(source[0].abspath)
     logfile = open(target[0].abspath, 'w')
     #    env['_venv'] = {target: venv}
