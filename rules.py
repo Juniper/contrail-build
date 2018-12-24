ff47280293e2d5098d47549479d6f08e5b1b2f4f
Michal Clapinski <michal.clapinski@codilime.com>
2018-12-24 21:58:04 +0000

    Enable C++ 11
    Change-Id: I63f6f43b1fc0a00cf3d2306134d9f6bd88d6148f
    Partial-Bug: #1809680
    Depends-On: I49c5e50032038a603740a13aebcf71dd8e19693a

--- rules.py
+++ rules.py
@@ -1390,6 +1390,8 @@ def SetupBuildEnvironment(conf):
         env.Append(CCFLAGS = ['/D_SCL_SECURE_NO_WARNINGS', '/D_CRT_SECURE_NO_WARNINGS'])
         # Stop Windows.h from including a lot of useless header files
         env.Append(CCFLAGS = '/DWIN32_LEAN_AND_MEAN')
+    else:
+        env.Append(CXXFLAGS = '-std=c++11')
 
     opt_level = env['OPT']
     if opt_level == 'production':
