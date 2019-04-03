40a27085c82436bbe4f43c98e6208f3e31621644
Ananth Suryanarayana <anantha@juniper.net>
2019-04-03 09:17:08 -0700

    run_coverage.py wrapper script to trim code coverage report
    Generate C++ code based coverage report using gcov and remove unnecessary
    noise from the report.
    
    Change-Id: Ie16235d56fccf2565c12359e5efa957fe185b203
    Partial-Jira-Bug: CEM-4442
    Depends-On: If97ec9b3cc33871f175bba84f5c4600ce99ee732

---
+++ generate_code_coverage_report.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2019 Juniper Networks, Inc. All rights reserved.
+#
+""" Run C++ based unit tests and generate code coverage report using gcov."""
+
+from __future__ import print_function
+from distutils.spawn import find_executable
+import os
+import re
+import shutil
+
+def generate_report():
+    """ Run the tests and generate report."""
+    if os.path.isdir("build/coverage"):
+        shutil.rmtree("build/coverage")
+    commands = '''
+scons -uij32 --optimization=coverage controller/cplusplus_test
+lcov --base-directory build/coverage --directory build/coverage -c -o build/coverage/controller_test.info
+genhtml -o build/coverage/controller/test_coverage -t test --num-spaces 4 build/coverage/controller_test.info
+'''
+    for cmd in commands.splitlines():
+        cmd_args = cmd.split()
+        if (len(cmd_args) == 0):
+            continue
+        cmd = cmd_args[0]
+        cmd_path = find_executable(cmd)
+        if not cmd_path:
+            continue
+        pid = os.fork()
+        if pid == 0:
+            # Avoid stdout buffering by execing command into child process.
+            os.execv(cmd_path, cmd_args)
+        os.waitpid(pid, 0)
+
+def fix_report(report):
+    """ Remove reports of generated code, test code and third-party code."""
+    if not os.path.isfile(report):
+        print("Error! Coverage report file %s not found!" % report)
+        return
+
+    shutil.move(report, report + ".orig")
+    exclude_list = [
+        r"boost",
+        r"build\/debug\/",
+        r"build\/coverage\/",
+        r"build\/include\/",
+        r"\/test",
+        r"usr\/",
+    ]
+
+    modify = []
+    with open(report + ".orig", "r") as filep:
+        lines = filep.readlines()
+        j = -1
+        i = -1
+        while True:
+            i += 1
+            if i >= len(lines):
+                break
+            exclude = False
+            for pattern in exclude_list:
+                if re.search(pattern, lines[i]):
+                    exclude = True
+                    break
+            if exclude:
+                j -= 1
+                i += 8
+                continue
+            j += 1
+            if j < len(modify):
+                modify[j] = lines[i]
+            else:
+                modify.append(lines[i])
+
+    with open(report, "w") as filep:
+        for i in range(j+1):
+            filep.write(modify[i])
+
+def print_help():
+    """ Print generated report access information."""
+    print("Archive generated report to a web server. e.g.")
+    print("rm -rf /cs-shared/contrail_code_coverage/test_coverage")
+    print("cp -a build/coverage/controller/test_coverage " +
+          "/cs-shared/contrail_code_coverage/")
+    print("http://10.84.5.100/cs-shared/contrail_code_coverage/test_coverage")
+
+def main():
+    """ Main routine."""
+    generate_report()
+    fix_report("build/coverage/controller/test_coverage/index.html")
+    fix_report("build/coverage/controller/test_coverage/index-sort-l.html")
+    fix_report("build/coverage/controller/test_coverage/index-sort-f.html")
+    print_help()
+
+main()
